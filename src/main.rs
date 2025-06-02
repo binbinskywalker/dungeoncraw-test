c0505c1720eee7aede4a98f4ee82ac9d5098ddc3
binbinskywalker <binbin2010qsc@msn.cn>
2025-06-02 21:47:46 +0800

    step3: use ecs system

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,19 @@
 mod map;
-mod player;
 mod map_builder;
 mod camera;
+mod components;
+mod spawner;
+mod systems;
 
 mod prelude {
+    pub use legion::*;
+    pub use legion::world::SubWorld;
+    pub use legion::systems::CommandBuffer;
+    pub use crate::components::*;
+
+    pub use crate::spawner::*;
+    pub use crate::systems::*;
+
     pub use bracket_lib::prelude::*;
     pub const SCREEN_WIDTH: i32=80;
     pub const  SCREEN_HEIGHT: i32=50;
@@ -11,34 +21,37 @@ mod prelude {
     pub const  DISPLAY_HEIGHT:i32= SCREEN_HEIGHT/2;
 
     pub use crate::map::*;
-    pub use crate::player::*;
     pub use crate::map_builder::*;
     pub use crate::camera::*;
 }
 
 use prelude::*;
+use systems::build_scheduler;
 
 struct State {
-    map: Map,
-    player: Player,
-    camera:Camera
+    ecs:World,
+    resources: Resources,
+    systems :Schedule
 }
 impl State {
-    // fn new() -> Self {
-    //     Self { 
-    //         map: Map::new(),
-    //         player: Player::new(
-    //             Point::new(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)),
-    //     }
-    // }
     fn new() -> Self {
+        let mut ecs=World::default();
+        let mut resources=Resources::default();
         let mut rng = RandomNumberGenerator::new();
 
-        let map_builder=MapBuilder::new(&mut rng);
-        Self{
-            map:map_builder.map,
-            player:Player::new(map_builder.player_start),
-            camera: Camera::new(map_builder.player_start)
+        let map_builder = MapBuilder::new(&mut rng);
+  
+        spawne_player(&mut ecs, map_builder.player_start);
+        map_builder.rooms.iter().skip(1).map(|r|r.center())
+        .for_each(|pos| spawn_monster(&mut ecs, &mut rng, pos));
+
+        resources.insert(map_builder.map);
+        resources.insert(Camera::new(map_builder.player_start));
+
+        Self { 
+            ecs, 
+            resources, 
+            systems: build_scheduler() 
         }
     }
 }
@@ -49,9 +62,10 @@ impl GameState for State {
         ctx.cls();
         ctx.set_active_console(1);
         ctx.cls();
-        self.player.update(ctx, &self.map, &mut self.camera);
-        self.map.render(ctx, &self.camera);
-        self.player.render(ctx, &self.camera);
+
+        self.resources.insert(ctx.key);
+        self.systems.execute(&mut self.ecs, &mut self.resources);
+        render_draw_buffer(ctx).expect("Render error");
     }
 }
 fn main() -> BError {
